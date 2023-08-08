729c595017a2455ef353eaf90f4ddb13b65e14e2
Kevin Restaino <github@kmr.io>
2023-08-08 12:59:49 -0700

    refactor: demo style tweaks

--- demo/src/App.js
+++ demo/src/App.js
@@ -131,7 +131,7 @@ function App() {
         ) : podcast.feedUrl ? (
           <section className="font-mono mt-8 max-w-5xl mx-auto">
             <h2 className="text-2xl font-bold">Podcast ({podcast.title})</h2>
-            <table className="mt-4 w-full bg-neutral-800">
+            <table className="mt-4 w-full bg-neutral-800 block overflow-scroll h-[35vh]">
               <thead className="bg-neutral-700">
                 <tr>
                   <th className="border border-neutral-600 p-2 font-bold">key</th>
@@ -148,33 +148,37 @@ function App() {
               </tbody>
             </table>
             <h2 className="text-2xl font-bold mt-4">Episodes ({episodes.length})</h2>
-            {episodes.map((episode, episodeIdx) => (
-              <table className="mt-4 w-full bg-neutral-800" key={episodeIdx}>
-                <thead className="bg-neutral-700">
-                  <tr>
-                    <th className="border border-neutral-600 p-2 font-bold">key</th>
-                    <th className="border border-neutral-600 p-2 font-bold">value</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {Object.keys(episode).map((key) => (
-                    <tr key={key}>
-                      <td className="border border-neutral-600 p-2">{key}</td>
-                      <td className="border border-neutral-600 p-2">{JSON.stringify(episode[key])}</td>
+            <div className="overflow-scroll h-[35vh]">
+              {episodes.map((episode, episodeIdx) => (
+                <table className="mt-4 w-full bg-neutral-800 block" key={episodeIdx}>
+                  <thead className="bg-neutral-700">
+                    <tr>
+                      <th className="border border-neutral-600 p-2 font-bold">key</th>
+                      <th className="border border-neutral-600 p-2 font-bold">value</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            ))}
+                  </thead>
+                  <tbody>
+                    {Object.keys(episode).map((key) => (
+                      <tr key={key}>
+                        <td className="border border-neutral-600 p-2">{key}</td>
+                        <td className="border border-neutral-600 p-2">{JSON.stringify(episode[key])}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              ))}
+            </div>
           </section>
         ) : null}
       </section>
 
-      <section className="max-w-5xl mx-auto my-8">
-        <div
-          className="prose prose-neutral dark:prose-invert max-w-5xl"
-          dangerouslySetInnerHTML={{ __html: readmeContent }}
-        />
+      <section className={podcast.feedUrl ? "bg-neutral-800" : ""}>
+        <div className="max-w-5xl mx-auto my-8 pt-8">
+          <div
+            className="prose prose-neutral dark:prose-invert max-w-5xl"
+            dangerouslySetInnerHTML={{ __html: readmeContent }}
+          />
+        </div>
       </section>
     </div>
   );
