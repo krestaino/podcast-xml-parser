6e03a1dc2cb03d90ad79b2bbd876bae986b7af3c
Kevin Restaino <github@kmr.io>
2023-08-11 23:08:52 -0700

    chore: bump version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,15 +1,15 @@
 import { type Podcast, type Episode, type Config } from "./types";
 export type { Podcast, Episode, Config };
 /**
- * Parses an XML podcast feed and returns a `Podcast` object.
+ * Parses a podcast's XML feed and returns structured data about the podcast and its episodes.
+ * Supports optional iTunes integration to retrieve additional details.
  *
- * @param xmlSource - The XML string or URL representing the podcast feed.
- *                    If an XML string is provided, it will be directly parsed as the XML content.
- *                    If a URL is provided, the XML content will be fetched from the URL before parsing.
- * @param config - A configuration object to specify pagination and other options.
- * @returns The parsed `Podcast` object.
+ * @param {string | URL | number} source - XML content, a URL pointing to the podcast feed, or an iTunes collectionId.
+ * @param {Config} [config] - Configuration options for parsing, like pagination or iTunes integration.
+ * @returns {Promise<{ podcast: Podcast; episodes: Episode[]; itunes?: any }>} Parsed podcast data.
+ * @throws {Error} Throws an error for invalid or empty XML feeds.
  */
-export default function podcastXmlParser(xmlSource: string | URL, config?: Config): Promise<{
+export default function podcastXmlParser(source: string | URL | number | any, config?: Config): Promise<{
     podcast: Podcast;
     episodes: Episode[];
     itunes?: any;
--- dist/index.js
+++ dist/index.js
@@ -39,24 +39,30 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var xmldom_1 = require("xmldom");
 var parser = new xmldom_1.DOMParser();
 /**
- * Preprocesses an XML string to handle entities and unclosed tags.
+ * Preprocesses an XML string to handle possible XML inconsistencies.
+ * Wraps content in a root tag if it doesn't start with one.
  *
- * @param xmlString - The XML string to preprocess.
- * @returns The preprocessed XML string.
+ * @param {string} xmlString - The XML string to preprocess.
+ * @returns {string} The preprocessed XML string.
  */
 function preprocessXml(xmlString) {
-    if (!xmlString.startsWith("<")) {
-        xmlString = "<root>".concat(xmlString, "</root>");
+    // Check if source is a valid XML string
+    if (xmlString.trim() === "") {
+        throw new Error("Empty XML feed. Please provide valid XML content.");
     }
-    var doc = parser.parseFromString(xmlString, "text/xml");
+    var wrappedString = xmlString.startsWith("<") ? xmlString : "<root>".concat(xmlString, "</root>");
+    var doc = parser.parseFromString(wrappedString, "text/xml");
     return new xmldom_1.XMLSerializer().serializeToString(doc);
 }
 /**
- * Fetches XML content from a URL using Fetch API.
+ * Retrieves XML content from a given URL using the Fetch API.
+ * Supports optional byte range requests.
  *
  * @param {string} url - The URL from which to fetch the XML content.
- * @returns {Promise<string>} A Promise that resolves to the XML content as a string.
- * @throws {Error} If there is an error fetching the XML content from the URL.
+ * @param {string} [range] - Optional range for byte requests.
+ * @param {boolean} [fetchEnd] - If true, fetch from the end of the range.
+ * @returns {Promise<string>} Resolves to the XML content as a string.
+ * @throws {Error} Throws an error if there's an issue fetching the XML content.
  */
 function fetchXmlFromUrl(url, range, fetchEnd) {
     return __awaiter(this, void 0, void 0, function () {
@@ -65,15 +71,15 @@ function fetchXmlFromUrl(url, range, fetchEnd) {
             switch (_a.label) {
                 case 0:
                     headers = {};
-                    if (range) {
-                        headers['Range'] = range;
+                    if (range !== null && range !== undefined && range.trim() !== "") {
+                        headers.Range = range;
                     }
                     return [4 /*yield*/, fetch(url, { headers: headers })];
                 case 1:
                     response = _a.sent();
-                    // You may want to check if partial content is returned
-                    if ((range || fetchEnd) && response.status !== 206) {
-                        throw new Error('Server does not support byte range requests.');
+                    // Check if partial content is returned
+                    if ((range !== null && range !== void 0 ? range : fetchEnd) !== undefined && response.status !== 206) {
+                        throw new Error("Server does not support byte range requests.");
                     }
                     return [4 /*yield*/, response.text()];
                 case 2: return [2 /*return*/, _a.sent()];
@@ -82,11 +88,11 @@ function fetchXmlFromUrl(url, range, fetchEnd) {
     });
 }
 /**
- * Retrieves the text content of a specified XML element.
+ * Extracts the text content from a specified XML element.
  *
- * @param element - The XML element to retrieve content from.
- * @param tagName - The tag name whose content is to be retrieved.
- * @returns The text content of the specified element's tag or an empty string if not found.
+ * @param {Element} element - The XML element to extract content from.
+ * @param {string} tagName - The name of the tag to retrieve content from.
+ * @returns {string} Text content of the tag, or an empty string if not found.
  */
 function getText(element, tagName) {
     var _a;
@@ -94,12 +100,47 @@ function getText(element, tagName) {
     return (_a = node === null || node === void 0 ? void 0 : node.textContent) !== null && _a !== void 0 ? _a : "";
 }
 /**
- * Helper function to create an `Episode` instance from an XML item element.
+ * Constructs a Podcast object based on the provided XML item element.
  *
- * @param item - The XML element representing an episode.
- * @returns The created `Episode` object.
+ * @param {Element} document - The XML element that represents a podcast.
+ * @returns {Podcast} The created Podcast object with parsed values.
  */
-function createEpisodeFromItem(item) {
+function createPodcast(document) {
+    var _a, _b, _c, _d, _e, _f;
+    var imageElem = document.getElementsByTagName("image")[0];
+    return {
+        copyright: getText(document, "copyright"),
+        contentEncoded: getText(document, "content:encoded"),
+        description: getText(document, "description"),
+        feedUrl: (_b = (_a = document.getElementsByTagName("atom:link")[0]) === null || _a === void 0 ? void 0 : _a.getAttribute("href")) !== null && _b !== void 0 ? _b : "",
+        image: {
+            link: getText(imageElem, "link"),
+            title: getText(imageElem, "title"),
+            url: getText(imageElem, "url"),
+        },
+        itunesAuthor: getText(document, "itunes:author"),
+        itunesCategory: (_d = (_c = document.getElementsByTagName("itunes:category")[0]) === null || _c === void 0 ? void 0 : _c.getAttribute("text")) !== null && _d !== void 0 ? _d : "",
+        itunesExplicit: getText(document, "itunes:explicit"),
+        itunesImage: (_f = (_e = document.getElementsByTagName("itunes:image")[0]) === null || _e === void 0 ? void 0 : _e.getAttribute("href")) !== null && _f !== void 0 ? _f : "",
+        itunesOwner: {
+            email: getText(document, "itunes:email"),
+            name: getText(document, "itunes:name"),
+        },
+        itunesSubtitle: getText(document, "itunes:subtitle"),
+        itunesSummary: getText(document, "itunes:summary"),
+        itunesType: getText(document, "itunes:type"),
+        language: getText(document, "language"),
+        link: getText(document, "link"),
+        title: getText(document, "title"),
+    };
+}
+/**
+ * Constructs an Episode object based on the provided XML item element.
+ *
+ * @param {Element} item - The XML element that represents an episode.
+ * @returns {Episode} The created Episode object with parsed values.
+ */
+function createEpisode(item) {
     var _a, _b;
     var enclosureElem = item.getElementsByTagName("enclosure")[0];
     return {
@@ -125,92 +166,127 @@ function createEpisodeFromItem(item) {
     };
 }
 /**
- * Parses an XML podcast feed and returns a `Podcast` object.
+ * Fetches podcast information from iTunes based on the provided podcast ID.
  *
- * @param xmlSource - The XML string or URL representing the podcast feed.
- *                    If an XML string is provided, it will be directly parsed as the XML content.
- *                    If a URL is provided, the XML content will be fetched from the URL before parsing.
- * @param config - A configuration object to specify pagination and other options.
- * @returns The parsed `Podcast` object.
+ * @param {number} id - The podcast ID for iTunes search.
+ * @returns {Promise<any | undefined>} The search results for the podcast from iTunes or undefined on error.
  */
-function podcastXmlParser(xmlSource, config) {
-    var _a, _b, _c, _d, _e, _f;
+function itunesLookup(id) {
+    return __awaiter(this, void 0, void 0, function () {
+        var itunesResponse, itunesData, err_1;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fetch("https://itunes.apple.com/lookup?id=".concat(id, "&entity=podcast"))];
+                case 1:
+                    itunesResponse = _a.sent();
+                    return [4 /*yield*/, itunesResponse.json()];
+                case 2:
+                    itunesData = _a.sent();
+                    return [2 /*return*/, itunesData.results[0]];
+                case 3:
+                    err_1 = _a.sent();
+                    throw new Error("Error fetching from iTunes.");
+                case 4: return [2 /*return*/];
+            }
+        });
+    });
+}
+/**
+ * Retrieves XML content from a given source, which can be a URL, iTunes ID, or an XML string.
+ *
+ * @param {string | URL | number} source - The source of the XML content, can be a URL object, an iTunes ID, or an XML string.
+ * @returns {Promise<{ itunes?: any, xmlString: string }>} Object containing iTunes data (if relevant) and the XML string.
+ * @throws {Error} Throws an error if the source type is invalid or if unable to fetch associated feed URL with the given iTunes ID.
+ */
+function retrieveXmlFromSource(source) {
+    return __awaiter(this, void 0, void 0, function () {
+        var xmlString, itunes, xmlString;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    if (!(source instanceof URL)) return [3 /*break*/, 2];
+                    return [4 /*yield*/, fetchXmlFromUrl(source.toString())];
+                case 1:
+                    xmlString = _a.sent();
+                    return [2 /*return*/, { xmlString: xmlString }];
+                case 2:
+                    if (!(typeof source === "number")) return [3 /*break*/, 6];
+                    return [4 /*yield*/, itunesLookup(source)];
+                case 3:
+                    itunes = _a.sent();
+                    if (!(typeof (itunes === null || itunes === void 0 ? void 0 : itunes.feedUrl) === "string")) return [3 /*break*/, 5];
+                    return [4 /*yield*/, fetchXmlFromUrl(itunes.feedUrl)];
+                case 4:
+                    xmlString = _a.sent();
+                    return [2 /*return*/, { itunes: itunes, xmlString: xmlString }];
+                case 5: 
+                // If iTunes ID is invalid or unable to fetch associated feed URL, throw an error
+                throw new Error("Invalid iTunes ID or unable to fetch associated feed URL.");
+                case 6:
+                    if (typeof source === "string") {
+                        // If source is already an XML string, return it directly
+                        return [2 /*return*/, { xmlString: source }];
+                    }
+                    else {
+                        // If the source type is none of the above, throw an error
+                        throw new Error("Invalid source type. Please provide a valid URL, iTunes ID, or XML string.");
+                    }
+                    _a.label = 7;
+                case 7: return [2 /*return*/];
+            }
+        });
+    });
+}
+/**
+ * Parses a podcast's XML feed and returns structured data about the podcast and its episodes.
+ * Supports optional iTunes integration to retrieve additional details.
+ *
+ * @param {string | URL | number} source - XML content, a URL pointing to the podcast feed, or an iTunes collectionId.
+ * @param {Config} [config] - Configuration options for parsing, like pagination or iTunes integration.
+ * @returns {Promise<{ podcast: Podcast; episodes: Episode[]; itunes?: any }>} Parsed podcast data.
+ * @throws {Error} Throws an error for invalid or empty XML feeds.
+ */
+function podcastXmlParser(source, config) {
     if (config === void 0) { config = {}; }
     return __awaiter(this, void 0, void 0, function () {
-        var xmlString, startChunk, preprocessedXml, doc, docElement, _g, start, limit, episodeElements, paginatedElements, episodes, imageElem, podcast, itunesResponse, itunes, err_1;
-        return __generator(this, function (_h) {
-            switch (_h.label) {
-                case 0:
-                    if (typeof xmlSource === "string" && xmlSource.trim() === "") {
-                        throw new Error("Empty XML feed. Please provide valid XML content.");
-                    }
-                    if (!(xmlSource instanceof URL)) return [3 /*break*/, 5];
-                    if (!config.requestSizeLimit) return [3 /*break*/, 2];
-                    return [4 /*yield*/, fetchXmlFromUrl(xmlSource.toString(), "bytes=0-".concat(config.requestSizeLimit))];
+        var _a, itunes, xmlString, preprocessedXml, doc, podcast, episodeElements, _b, start, limit, paginatedElements, episodes, itunesResponse, err_2;
+        return __generator(this, function (_c) {
+            switch (_c.label) {
+                case 0: return [4 /*yield*/, retrieveXmlFromSource(source)];
                 case 1:
-                    startChunk = _h.sent();
-                    xmlString = startChunk + "</channel></rss>";
-                    return [3 /*break*/, 4];
-                case 2: return [4 /*yield*/, fetchXmlFromUrl(xmlSource.toString())];
-                case 3:
-                    xmlString = _h.sent();
-                    _h.label = 4;
-                case 4: return [3 /*break*/, 6];
-                case 5:
-                    xmlString = xmlSource;
-                    _h.label = 6;
-                case 6:
+                    _a = _c.sent(), itunes = _a.itunes, xmlString = _a.xmlString;
                     preprocessedXml = preprocessXml(xmlString);
                     doc = parser.parseFromString(preprocessedXml, "text/xml");
-                    docElement = doc.documentElement;
-                    _g = config.start, start = _g === void 0 ? 0 : _g, limit = config.limit;
+                    podcast = createPodcast(doc.documentElement);
                     episodeElements = Array.from(doc.getElementsByTagName("item"));
+                    _b = config.start, start = _b === void 0 ? 0 : _b, limit = config.limit;
                     paginatedElements = limit !== undefined ? episodeElements.slice(start, start + limit) : episodeElements;
-                    episodes = paginatedElements.map(createEpisodeFromItem);
-                    imageElem = docElement.getElementsByTagName("image")[0];
-                    podcast = {
-                        copyright: getText(docElement, "copyright"),
-                        contentEncoded: getText(doc.documentElement, "content:encoded"),
-                        description: getText(doc.documentElement, "description"),
-                        feedUrl: xmlSource instanceof URL
-                            ? xmlSource.toString()
-                            : (_b = (_a = doc.getElementsByTagName("atom:link")[0]) === null || _a === void 0 ? void 0 : _a.getAttribute("href")) !== null && _b !== void 0 ? _b : "",
-                        image: {
-                            link: getText(imageElem, "link"),
-                            title: getText(imageElem, "title"),
-                            url: getText(imageElem, "url"),
-                        },
-                        itunesAuthor: getText(doc.documentElement, "itunes:author"),
-                        itunesCategory: (_d = (_c = doc.getElementsByTagName("itunes:category")[0]) === null || _c === void 0 ? void 0 : _c.getAttribute("text")) !== null && _d !== void 0 ? _d : "",
-                        itunesExplicit: getText(doc.documentElement, "itunes:explicit"),
-                        itunesImage: (_f = (_e = doc.getElementsByTagName("itunes:image")[0]) === null || _e === void 0 ? void 0 : _e.getAttribute("href")) !== null && _f !== void 0 ? _f : "",
-                        itunesOwner: {
-                            email: getText(doc.documentElement, "itunes:email"),
-                            name: getText(doc.documentElement, "itunes:name"),
-                        },
-                        itunesSubtitle: getText(doc.documentElement, "itunes:subtitle"),
-                        itunesSummary: getText(doc.documentElement, "itunes:summary"),
-                        itunesType: getText(doc.documentElement, "itunes:type"),
-                        language: getText(doc.documentElement, "language"),
-                        link: getText(doc.documentElement, "link"),
-                        title: getText(doc.documentElement, "title"),
-                    };
-                    if (!config.itunes) return [3 /*break*/, 11];
-                    _h.label = 7;
-                case 7:
-                    _h.trys.push([7, 10, , 11]);
+                    episodes = paginatedElements.map(createEpisode);
+                    if (!(config.itunes === true)) return [3 /*break*/, 7];
+                    _c.label = 2;
+                case 2:
+                    _c.trys.push([2, 6, , 7]);
+                    if (!(itunes === null || itunes === undefined)) return [3 /*break*/, 5];
                     return [4 /*yield*/, fetch("https://itunes.apple.com/search?term=".concat(podcast.title, "&entity=podcast"))];
-                case 8:
-                    itunesResponse = _h.sent();
+                case 3:
+                    itunesResponse = _c.sent();
                     return [4 /*yield*/, itunesResponse.json()];
-                case 9:
-                    itunes = _h.sent();
+                case 4:
+                    itunes = _c.sent();
+                    // Set podcast if the feedUrl is equal on iTunes and in the XML
                     itunes = itunes.results.find(function (result) { return result.feedUrl === podcast.feedUrl; });
-                    return [2 /*return*/, { itunes: itunes, podcast: podcast, episodes: episodes }];
-                case 10:
-                    err_1 = _h.sent();
-                    return [3 /*break*/, 11];
-                case 11: return [2 /*return*/, { podcast: podcast, episodes: episodes }];
+                    _c.label = 5;
+                case 5: 
+                // All done, return data
+                return [2 /*return*/, { itunes: itunes, podcast: podcast, episodes: episodes }];
+                case 6:
+                    err_2 = _c.sent();
+                    throw new Error("Error fetching from iTunes.");
+                case 7: 
+                // All done, return data
+                return [2 /*return*/, { podcast: podcast, episodes: episodes }];
             }
         });
     });
