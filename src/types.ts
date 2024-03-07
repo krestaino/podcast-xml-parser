519b5feba837bf111f3c4d1f45f69a27e75878b0
Kevin Restaino <kevinrestaino@gmail.com>
2024-03-06 21:08:38 -0500

    feat: new Itunes type

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,26 @@
 /**
+ * Represents the podcast.
+ */
+export interface Podcast {
+  copyright: string;
+  contentEncoded: string;
+  description: string;
+  feedUrl: string;
+  image: { link: string; title: string; url: string } | null;
+  itunesAuthor: string;
+  itunesCategory: string | null;
+  itunesExplicit: string;
+  itunesImage: string | null;
+  itunesOwner: { name: string; email: string } | null;
+  itunesSubtitle: string;
+  itunesSummary: string;
+  itunesType: string;
+  language: string;
+  link: string;
+  title: string;
+}
+
+/**
  * Represents an individual podcast episode.
  */
 export interface Episode {
@@ -21,28 +43,6 @@ export interface Episode {
 }
 
 /**
- * Represents a podcast with multiple episodes.
- */
-export interface Podcast {
-  copyright: string;
-  contentEncoded: string;
-  description: string;
-  feedUrl: string;
-  image: { link: string; title: string; url: string } | null;
-  itunesAuthor: string;
-  itunesCategory: string | null;
-  itunesExplicit: string;
-  itunesImage: string | null;
-  itunesOwner: { name: string; email: string } | null;
-  itunesSubtitle: string;
-  itunesSummary: string;
-  itunesType: string;
-  language: string;
-  link: string;
-  title: string;
-}
-
-/**
  * Represents the config.
  */
 export interface Config {
@@ -51,4 +51,42 @@ export interface Config {
   requestHeaders?: Record<string, string>;
   requestSize?: number;
   itunes?: boolean;
+}
+
+/**
+ * Represents the additional details that can be fetched from iTunes.
+ */
+export interface Itunes {
+  wrapperType?: string;
+  kind?: string;
+  collectionId?: number;
+  trackId?: number;
+  artistName?: string;
+  collectionName?: string;
+  trackName?: string;
+  collectionCensoredName?: string;
+  trackCensoredName?: string;
+  collectionViewUrl?: string;
+  feedUrl?: string;
+  trackViewUrl?: string;
+  artworkUrl30?: string;
+  artworkUrl60?: string;
+  artworkUrl100?: string;
+  artworkUrl600?: string;
+  collectionPrice?: number;
+  trackPrice?: number;
+  trackRentalPrice?: number;
+  collectionHdPrice?: number;
+  trackHdPrice?: number;
+  trackHdRentalPrice?: number;
+  releaseDate?: string;
+  collectionExplicitness?: string;
+  trackExplicitness?: string;
+  trackCount?: number;
+  country?: string;
+  currency?: string;
+  primaryGenreName?: string;
+  contentAdvisoryRating?: string;
+  genreIds?: string[];
+  genres?: string[];
 }
