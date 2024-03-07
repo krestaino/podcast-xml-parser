9b56a459f9358f02df7c8b110e17366ba7d1f6a1
krestaino <github@kmr.io>
2024-03-07 00:18:28 -0500

    refactor: use Itunes type

--- dist/itunes.d.ts
+++ dist/itunes.d.ts
@@ -1,7 +1,8 @@
+import { type Itunes } from "./types";
 /**
  * Fetches podcast information from iTunes based on the provided podcast ID.
  *
  * @param id - The podcast ID for iTunes search.
  * @returns The search results for the podcast from iTunes or undefined on error.
  */
-export declare function itunesLookup(id: number): Promise<any | undefined>;
+export declare function itunesLookup(id: number): Promise<Itunes | undefined>;
--- src/itunes.ts
+++ src/itunes.ts
@@ -1,10 +1,12 @@
+import { type Itunes } from "./types";
+
 /**
  * Fetches podcast information from iTunes based on the provided podcast ID.
  *
  * @param id - The podcast ID for iTunes search.
  * @returns The search results for the podcast from iTunes or undefined on error.
  */
-export async function itunesLookup(id: number): Promise<any | undefined> {
+export async function itunesLookup(id: number): Promise<Itunes | undefined> {
   const itunesResponse = await fetch(`https://itunes.apple.com/lookup?id=${id}&entity=podcast`);
   const itunesData = await itunesResponse.json();
   return itunesData.results[0];
